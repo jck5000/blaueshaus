6ef410dc5229619a31717afe1212e9882c86c88c
jck5000 <107056028+jck5000@users.noreply.github.com>
2025-05-01 18:31:35 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,13 +4,13 @@
 # chmod +x /etc/kiosk/env.sh
 
 # Eigene IP-Adresse (PVE-Host)
-export KIOSK_PVE_IP="192.168.188.186"   # ← ändern, falls sich IP ändert
+export KIOSK_PVE_IP="192.168.188.174"   # ← ändern, falls sich IP ändert
 
 # Home Assistant IP-Adresse (VM oder Container)
 export KIOSK_HA_IP="192.168.188.76"     # ← ändern, wenn sich HA-Adresse ändert
 
 # Bildschirmanschlussname (für xrandr und xset dpms)
-export KIOSK_DISPLAY_PORT="DisplayPort-1"  # z. B. HDMI-1, DP-1, etc.
+export KIOSK_DISPLAY_PORT="DisplayPort-0"  # z. B. HDMI-1, DP-1, etc.
 
 # (Optional) Standard-URL im Kioskmodus (z. B. Home Assistant Dashboard)
 export KIOSK_URL="http://$KIOSK_HA_IP:8123/lovelace/kiosk?kiosk"
